41d08db2e3c34d1085a0674cc815118b497494e3
itaygil3 <itaygil49@gmail.com>
2017-03-27 16:38:24 +0300

    adding all files

--- Torch/app/src/main/java/com/magshimim/torch/FrameShower.java
+++ Torch/app/src/main/java/com/magshimim/torch/FrameShower.java
@@ -1,18 +1,21 @@
 package com.magshimim.torch;
 
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
 import android.graphics.Bitmap;
 
 public class FrameShower extends AppCompatActivity {
-
-    @Override
+    private boolean DEBUG = BuildConfig.DEBUG;
+    private final static String TAG = "FrameShower";
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_frame_shower);
@@ -28,8 +31,16 @@ public class FrameShower extends AppCompatActivity {
             }
         });
         byte[] toFrame = getIntent().getByteArrayExtra("frame");
+        if (DEBUG)
+        {
+            Log.d(TAG,"Bitmap Size: "+toFrame.length);
+        }
         Bitmap bmp = BitmapFactory.decodeByteArray(toFrame,0,toFrame.length);
-        
+        Canvas c = new Canvas(bmp);
+        if (DEBUG)
+        {
+            Log.d(TAG,"canvas has been created");
+        }
     }
 
 }
