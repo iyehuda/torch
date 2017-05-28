d692f0b8545bb323cb51a9302fab35f5c72801e8
iYehuda <yehudaac1@gmail.com>
2017-05-28 17:17:48 +0300

    Mirroring in action
    The server can now mirror varint delimited messages from a sender to a
    receiver. The receiver connects first.

--- TorchServer/TorchServer/mirroring.js
+++ TorchServer/TorchServer/mirroring.js
@@ -6,9 +6,10 @@ var varint = require('varint');
 function MirroringClient(socket) {
     events.EventEmitter.call(this);
     var self = this;
-    this.currentLength = undefined;
-    this.currentMessage = undefined;
-    this.currentOffset = undefined;
+    this.currentLength = null;
+    this.currentMessage = null;
+    this.currentOffset = null;
+    this.name = socket.name;
     this.socket = socket;
     socket.on('data', function (data) {
         self.handleMessage(data);
@@ -18,18 +19,19 @@ function MirroringClient(socket) {
     });
     socket.on('close', function (had_error) {
         console.log(`${self.toString()} disconnected`);
+        self.emit('close');
     });
 }
 
 util.inherits(MirroringClient, events.EventEmitter);
 
 MirroringClient.prototype.toString = function toString() {
-    return this.socket.name;
+    return this.name || 'unknown socket';
 };
 
 MirroringClient.prototype.handleMessage = function handleMessage(data) {
-    if (this.currentLength == undefined) {
-        if (this.currentMessage == undefined) {
+    if (this.currentLength == null) {
+        if (this.currentMessage == null) {
             try {
                 this.currentLength = varint.decode(data);
                 this.currentOffset = 0;
@@ -43,12 +45,12 @@ MirroringClient.prototype.handleMessage = function handleMessage(data) {
         }
         else {
             data = Buffer.concat(this.currentMessage, data);
-            this.currentMessage = undefined;
+            this.currentMessage = null;
             this.handleMessage(data);
         }
     }
     else {
-        if (this.currentMessage == undefined) {
+        if (this.currentMessage == null) {
             this.currentMessage = Buffer.alloc(this.currentLength);
             this.currentOffset = 0;
         }
@@ -58,20 +60,78 @@ MirroringClient.prototype.handleMessage = function handleMessage(data) {
 
         if (this.currentOffset == this.currentLength) {
             this.emit('message', this.currentMessage);
-            this.currentLength = undefined;
-            this.currentMessage = undefined;
-            this.currentOffset = undefined;
+            this.currentLength = null;
+            this.currentMessage = null;
+            this.currentOffset = null;
             if (dataOffset < data.length) {
-                data = Buffer.slice(data, dataOffset);
+                data = data.slice(dataOffset);
                 this.handleMessage(data);
             }
         }
     }
 };
 
+MirroringClient.prototype.send = function send(message) {
+    this.socket.write(Buffer.from(varint.encode(message.length)));
+    this.socket.write(message);
+};
+
+MirroringClient.prototype.destroy = function destroy() {
+    this.currentLength = null;
+    this.currentOffset = null;
+    this.currentMessage = null;
+    if (this.socket != null) {
+        if (!this.socket.destroyed)
+            this.socket.destroy();
+        this.socket = null;
+    }
+};
+
+function MirroringSession() {
+    var self = this;
+    this.closeCallback = function () {
+        self.destroy();
+    };
+    this.sender = null;
+    this.receiver = null;
+}
+
+MirroringSession.prototype.isFull = function isFull() {
+    return this.sender != null && this.receiver != null;  
+}
+
+MirroringSession.prototype.addClient = function addClient(client) {
+    var self = this;
+    client.on('close', this.closeCallback);
+    if (this.receiver == null) {
+        this.receiver = client;
+    }
+    else if (this.sender == null) {
+        this.sender = client;
+        this.sender.on('message', function (message) {
+            console.log(`sending ${message.length} bytes to ${self.receiver.toString()}`);
+            self.receiver.send(message);
+        });
+    }
+}
+
+MirroringSession.prototype.destroy = function destroy() {
+    if (this.receiver != null) {
+        this.receiver.removeListener('close', this.closeCallback);
+        this.receiver.destroy();
+        this.receiver = null;
+    }
+    if (this.sender != null) {
+        this.sender.removeListener('close', this.closeCallback);
+        this.sender.destroy();
+        this.sender = null;
+    }
+}
+
 function MirroringServer() {
     var self = this;
     this.port = 27015;
+    this.sessions = [];
     this.server = net.createServer(function (socket) {
         socket.name = `${socket.remoteAddress}:${socket.remotePort}`;
         console.log(`${socket.name} connected`);
@@ -84,6 +144,10 @@ MirroringServer.prototype.addClient = function addClient(socket) {
     client.on('message', function (message) {
         console.log('received message at size ' + message.length);
     });
+    if (this.sessions.length == 0 || this.sessions[this.sessions.length - 1].isFull()) {
+        this.sessions.push(new MirroringSession());
+    }
+    this.sessions[this.sessions.length - 1].addClient(client);
 };
 
 MirroringServer.prototype.listen = function listen() {
