94776fe6bc6be7e0da587de7ae9f1dc6f2a04d96
iYehuda <yehudaac1@gmail.com>
2017-05-28 14:48:16 +0300

    varint parsing and message buffering
    The server now can receive varint delimited messages and buffer the
    until completely received.
    Imported 'varint' package

--- TorchServer/TorchServer/mirroring.js
+++ TorchServer/TorchServer/mirroring.js
@@ -1,10 +1,17 @@
-﻿var net = require('net');
+﻿var events = require('events');
+var net = require('net');
+var util = require('util');
+var varint = require('varint');
 
 function MirroringClient(socket) {
+    events.EventEmitter.call(this);
     var self = this;
+    this.currentLength = undefined;
+    this.currentMessage = undefined;
+    this.currentOffset = undefined;
     this.socket = socket;
     socket.on('data', function (data) {
-        console.log(`${self.toString()} sent ${data.length} bytes`);
+        self.handleMessage(data);
     });
     socket.on('error', function (error) {
         console.log(`${self.toString} had an error: ${error}`);
@@ -14,8 +21,52 @@ function MirroringClient(socket) {
     });
 }
 
+util.inherits(MirroringClient, events.EventEmitter);
+
 MirroringClient.prototype.toString = function toString() {
     return this.socket.name;
+};
+
+MirroringClient.prototype.handleMessage = function handleMessage(data) {
+    if (this.currentLength == undefined) {
+        if (this.currentMessage == undefined) {
+            try {
+                this.currentLength = varint.decode(data);
+                this.currentOffset = 0;
+                data = data.slice(varint.decode.bytes);
+                this.handleMessage(data);
+            }
+            catch (error) {
+                console.error(error);
+                this.currentMessage = data;
+            }
+        }
+        else {
+            data = Buffer.concat(this.currentMessage, data);
+            this.currentMessage = undefined;
+            this.handleMessage(data);
+        }
+    }
+    else {
+        if (this.currentMessage == undefined) {
+            this.currentMessage = Buffer.alloc(this.currentLength);
+            this.currentOffset = 0;
+        }
+
+        var dataOffset = data.copy(this.currentMessage, this.currentOffset);
+        this.currentOffset += dataOffset;
+
+        if (this.currentOffset == this.currentLength) {
+            this.emit('message', this.currentMessage);
+            this.currentLength = undefined;
+            this.currentMessage = undefined;
+            this.currentOffset = undefined;
+            if (dataOffset < data.length) {
+                data = Buffer.slice(data, dataOffset);
+                this.handleMessage(data);
+            }
+        }
+    }
 };
 
 function MirroringServer() {
@@ -30,12 +81,15 @@ function MirroringServer() {
 
 MirroringServer.prototype.addClient = function addClient(socket) {
     var client = new MirroringClient(socket);
+    client.on('message', function (message) {
+        console.log('received message at size ' + message.length);
+    });
 };
 
 MirroringServer.prototype.listen = function listen() {
     var self = this;
     this.server.listen(this.port, function () {
-        console.log(`istening on port ${self.port}`);
+        console.log(`listening on port ${self.port}`);
     });
 }
 
